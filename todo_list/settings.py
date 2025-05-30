6e1563609f078600a62eeaa86cb9674c91f481a7
Yalkap010 <Yalkapmyradow006@gmail.com>
2025-05-30 23:34:03 +0500

    Update settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -123,6 +123,7 @@ ALLOWED_HOSTS = ['your-service-name.onrender.com']
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+MEDIA_URL = '/media/'
+  MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # fix
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
